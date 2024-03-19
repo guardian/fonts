1082ae27827e28cdcbb2e6f2c7bc721a6950fb6b
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2024-03-19 13:43:45 +0000

    add GuardianFont and add modifier

---
+++ Sources/GuardianFonts/GuardianFont.swift
@@ -0,0 +1,22 @@
+import Foundation
+import SwiftUI
+
+public struct GuardianFont {
+    public let style: GuardianFontStyle
+    public let size: CGFloat
+    public let lineHeight: CGFloat?
+
+    public var font: Font {
+        Font.custom(style.fontName, size: size)
+    }
+
+    public var uiFont: UIFont {
+        UIFont(name: style.fontName, size: size) ?? .systemFont(ofSize: size)
+    }
+
+    public init(style: GuardianFontStyle, size: CGFloat, lineHeight: CGFloat? = nil) {
+        self.style = style
+        self.size = size
+        self.lineHeight = lineHeight
+    }
+}
